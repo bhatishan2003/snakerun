91362e1d2fe13d76a5a5f005c7427f7a4c82dbfc
Ishan <ishan2003bhat@gmail.com>
2025-08-31 20:51:53 +0530

    chore: bump  version 0.0.1 -> 0.0.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ dev_requires = read_requirements("requirements_dev.txt")
 
 setup(
     name="snakerun",
-    version="0.0.1",
+    version="0.0.2",
     author="Ishan Bhat",
     author_email="ishan2003bhat@gmail.com",
     description="A Python based CLI game for playing classical snake game",
